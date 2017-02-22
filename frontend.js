1bed3c09bc59a5a5ca3a60d56b1d9f5c49748a20
jaylensoeur <jaylensoeur@gmail.com>
2017-02-22 11:30:16 +1100

    added rule for globals (#1)
    - upped version

--- frontend.js
+++ frontend.js
@@ -1,6 +1,7 @@
 module.exports = {
     rules: {
-        'no-var': 0 // http://eslint.org/docs/rules/no-var we are not using es6 on client side
+        'no-var': 0, // http://eslint.org/docs/rules/no-var we are not using es6 on client side
+        'no-undef': 2
     },
     extends: 'etaskr/common'
 };
